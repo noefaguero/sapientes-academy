4e3277ddf394d055cf903b4f3803e89ca46e6757
noefaguero <noe.fernandez.aguero@gmail.com>
2023-11-25 12:15:56 +0100

    Structure, stylesheet functionality of the courses section

--- js/index.js
+++ js/index.js
@@ -1,18 +1,36 @@
 const panel = document.getElementById("panelcontainer")
 const tab = document.getElementById("tab")
 let j = 0
+const courses = document.getElementById("courses")
+const courses_item = document.querySelectorAll(".course")
 
 tab.addEventListener("click", (event) => {
 
-if (event.target.tagName === "BUTTON") {
-    tab.children[j].classList.remove("clicked")
-    event.target.classList.add("clicked")
+    if (event.target.tagName === "BUTTON") {
+        tab.children[j].classList.remove("clicked")
+        event.target.classList.add("clicked")
 
-    panel.children[j].classList.remove("active")
-    let i = Array.from(tab.children).indexOf(event.target)
-    panel.children[i].classList.add("active")
+        panel.children[j].classList.remove("active")
+        let i = Array.from(tab.children).indexOf(event.target)
+        panel.children[i].classList.add("active")
+        
+        j = i
+    }
     
-    j = i
-}
+})
+
+courses.addEventListener("click", (event) => {
     
+    if (event.target.className == "course clickable") {
+        courses_item.forEach(course => course.lastElementChild.classList.remove("collapse"))
+        event.target.lastElementChild.classList.add("collapse")
+    } else {
+        courses_item.forEach(course => {
+            let element = course.lastElementChild
+            if (element.classList.contains("collapse")) {
+                element.classList.remove("collapse")
+            }
+        })
+        event.target.closest('.course').lastElementChild.classList.add("collapse"); // el elemento mas cercano
+    }
 })
